90fbfebd498a5df79f832a3a18e33cf4012fedb1
輪•|ʀɪɴ+ᴋᴇɴᴏ࿐ <robinaaspirant@gmail.com>
2024-03-31 14:46:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['918787599649', 'RIN STORE MLBB', true],
+  ['918731030752', '༆ᴍʀ ᴀɴsᴡᴇʀ༒ᴏғғɪᴄɪᴀʟ ᴇᴅɪᴛ࿐', true], 
   [''] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "918787599649" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['918787599649'] 
+global.prems = ['918787599649', '918731030752', '919398758484']
+global.allowed = ['917605902011', '918731030752', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,16 +50,16 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'RIN BOT OFFICIAL'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'RIN STORE MLBB' 
+global.author = '༆ᴍʀ ᴀɴsᴡᴇʀ༒ᴏғғɪᴄɪᴀʟ ᴇᴅɪᴛ࿐' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/mr_answer_official\n' 
+global.dygp = 'https://chat.whatsapp.com/DQQf1GMuVhh4fuIIycwfRd'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.fgyt = 'https://youtube.com/@Mr_Answer_Official'
+global.fgpyp = 'https://youtube.com/@Mr_Answer_Official'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
